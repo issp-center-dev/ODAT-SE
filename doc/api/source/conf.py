0eb91d95e16a9bcc82b5ad5d9f11501e21109111
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2024-10-31 11:27:23 +0900

    sphinx-apidoc

---
+++ doc/api/source/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import sys
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ODAT-SE API'
+copyright = '2020-, ISSP, UTokyo'
+author = '2DMAT Developers'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+sys.path.insert(0, "../../src")
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.todo",
+    "sphinx.ext.napoleon",
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "haiku"
+html_static_path = ['_static']
+
+# -- Options for todo extension ----------------------------------------------
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
+
+autoclass_content = 'both'
+
+napoleon_include_init_with_doc = True
+
---
+++ doc/make.sh
@@ -0,0 +1,29 @@
+if [ "_$1" == "_--latex" ]; then
+  BUILD_LATEX=ON
+  latexmk --version > /dev/null 2>&1 || { echo "Latexmk is not installed"; exit 1; }
+else
+  BUILD_LATEX=OFF
+fi
+
+sphinx-build --version > /dev/null 2>&1 || { echo "Sphinx is not installed"; exit 1; }
+
+DOCROOT=`pwd`
+
+# user's manual
+cd ${DOCROOT}/ja
+make html
+if [ $BUILD_LATEX = "ON" ]; then
+  make latexpdf
+fi
+
+cd ${DOCROOT}/en
+make html
+if [ $BUILD_LATEX = "ON" ]; then
+  make latexpdf
+fi
+
+# api
+cd $DOCROOT
+sphinx-apidoc -P -f -e -o ./api/source ../src/odatse/
+cd api
+make html
