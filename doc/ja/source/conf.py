cb560eec6aecc0241a9829472873ae10558d011f
T.Aoyama <aoym@issp.u-tokyo.ac.jp>
2024-11-29 10:31:15 +0900

    update version info in manual

--- doc/ja/source/conf.py
+++ doc/ja/source/conf.py
@@ -23,7 +23,7 @@ author = u'2DMAT Developer team'
 
 version = '3.0'
 # The full version, including alpha/beta/rc tags
-release = '3.0-dev'
+release = '3.0.0'
 
 # -- General configuration ---------------------------------------------------
 
